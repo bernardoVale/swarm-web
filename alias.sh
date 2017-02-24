84b54be91b6b2d5c1def0af97d13232c92b4b6f3
Bernardo Vale <bvale@wsgc.com>
2017-02-23 22:44:01 -0300

    few more scripts

---
+++ alias.sh
@@ -0,0 +1,3 @@
+alias master="eval $(docker-machine env swmaster)"
+alias worker1="eval $(docker-machine env swworker01)"
+alias worker2="eval $(docker-machine env swworker02)"
---
+++ push.sh
@@ -0,0 +1,4 @@
+docker push localhost:5000/site-prd-back
+docker push localhost:5000/site-prd-front
+docker push localhost:5000/site-prd-acms
+docker push localhost:5000/mongo
